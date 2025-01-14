e6857c25eafd54d802f0a4329273791ed0e9c1e5
Patrick510 <patrickdedinho321xz@gmail.com>
2025-01-14 17:30:02 -0400

    Ex03 done

--- README.md
+++ README.md
@@ -18,24 +18,3 @@ Tempo: 30 minutos. Tempo não é longo, portanto, o foco deve ser em funcionalid
 Aprendizado: O objetivo principal é praticar os conceitos aprendidos e explorar novas possibilidades com o Flask.
 Ferramentas: Recomenda-se utilizar um editor de código com suporte a Python e Flask, como o Visual Studio Code.
 Proibido usar Internet.
-
-## 3. Página de upload de fotos:
-
-Objetivo da atividade: Criar uma aplicação simples para fazer upload de imagens.
-
-### Funcionalidades:
-
-- Formulário para escolher o arquivo a partir do computador do usuário.
-- Armazenar os arquivos enviados pelos usuários em uma pasta.
-- Utilização de templates para formatar a página.- Retornar mensagens de sucesso ou falha na autenticação. Em caso de sucesso, deve-se retornar também o path de onde o arquivo foi armazenado no servidor.
-
-### Dicas:
-
-- Utilizar lista para armazenar os arquivos enviados.
-- Usar o módulo datetime para registrar a data e hora do upload do arquivo.
-- Criar um template básico com HTML e CSS para estilizar a página.
-
-Tempo: 30 minutos. Tempo não é longo, portanto, o foco deve ser em funcionalidades básicas.
-Aprendizado: O objetivo principal é praticar os conceitos aprendidos e explorar novas possibilidades com o Flask.
-Ferramentas: Recomenda-se utilizar um editor de código com suporte a Python e Flask, como o Visual Studio Code.
-Proibido usar Internet.
--- app/app.py
+++ app/app.py
@@ -9,28 +9,29 @@ validate = ["user", "admin"]
 
 @app.route("/")
 def home():
-  return render_template("index.html", name="Login")
+    return render_template("index.html", name="Login")
 
 @app.route("/login", methods=["POST"])
 def login():
-  global attempt
-  user = request.form.get("user")
-  password = request.form.get("pass")
-  
-  if attempt >= 3:
-    return render_template("index.html", message=message[2], disable="true")
-  
-  if user and password:
-    if user == validate[0] and password == validate[1]:
-      attempt += 1
-      attempts.append({"attempt":attempt, "user": user, "date":datetime.now().strftime("%Y-%m-%d")})
-      return render_template("index.html", message=message[0], disable="false")
-    else:
-      attempt += 1
-      attempts.append({"attempt":attempt, "user": user, "date":datetime.now().strftime("%Y-%m-%d")})
-      return render_template("index.html", message=message[1], disable="true")
+    global attempt
+    user = request.form.get("user")
+    password = request.form.get("pass")
+    
+    if attempt == 3:
+        return render_template("index.html", message=message[2], disable=True)
+    
+    if user and password:
+        if user == validate[0] and password == validate[1]:
+            attempts.append({"attempt": attempt, "user": user, "date": datetime.now().strftime("%Y-%m-%d")})
+            attempt = 0 
+            return render_template("index.html", message=message[0], disable=False)
+        else:
+            attempt += 1
+            attempts.append({"attempt": attempt, "user": user, "date": datetime.now().strftime("%Y-%m-%d")})
+            return render_template("index.html", message=message[1])
 
-  return render_template("index.html", message=message[3], disable="false")
+    attempt = 0
+    return render_template("index.html", message=message[3], disable=False)
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
