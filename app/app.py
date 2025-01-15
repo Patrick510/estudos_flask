c6dd4251a1e24a1ced9d62275dd80290011b9fc0
Patrick510 <patrickdedinho321xz@gmail.com>
2025-01-14 20:57:00 -0400

    Refactor image upload functionality in app.py and update form.html for file input

--- app/app.py
+++ app/app.py
@@ -1,30 +1,23 @@
+import os
 from flask import Flask, render_template, request
 from datetime import datetime
 
 app = Flask(__name__)
-id_text = 0
-texts = []
 
 @app.route("/")
 def form():
-  return render_template('form.html', name="Form")
-
-@app.route("/home")
-def home():
-  return render_template("index.html", name="Home", texts=texts)
+    return render_template('form.html', name="Upload de Imagem")
 
 @app.route("/submit", methods=["POST"])
 def submit():
-  global id_text
-  text = request.form.get("text")
-  if text:
-    id_text+=1
-    date = datetime.now().strftime("%Y-%m-%d")
-    texts.append({"id": id_text, "text":text, "date":date})
-    message = "Texto salvo com sucesso"
-  else:
-    message = "Texto nao salvo"
-  return render_template("form.html", name="Form", message=message, texts=texts)
+    imagem = request.files['image'] 
+
+    if imagem:
+        caminho = f"./uploads/{imagem.filename.split('.')[0]}_{datetime.now().strftime('%Y-%m-%d')}.{imagem.filename.split('.')[-1]}"  
+        os.makedirs('./uploads', exist_ok=True) 
+        imagem.save(caminho)
+        return render_template('form.html', name="Upload de Imagem", message=f"Imagem salva em {caminho}")
+    return "Nenhuma imagem recebida"
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
